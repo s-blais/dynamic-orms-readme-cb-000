62deec791f17f9607cc1b204a73d43f6171a5de6
Stephen Blais <stephen@s-blais.com>
2019-10-10 17:41:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   def values_for_insert
     values = []
     self.class.column_names.each do |col|
-      values << "'#{send(col)}'" unless send(col_name).nil?
+      values << "'#{send(col)}'" unless send(col).nil?
       end
     values.join(",")
   end
