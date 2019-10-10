d2eb304d1bff482bfbad77992291508e68f75e31
Stephen Blais <stephen@s-blais.com>
2019-10-10 18:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)"
 
-    DB[:conn].execute(sql, "#{values_for_insert}")
+    DB[:conn].execute(sql, "#{[values_for_insert]}")
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
