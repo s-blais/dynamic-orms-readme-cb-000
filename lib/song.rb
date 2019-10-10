e03b5d178541d57568dcd75d51ce554787d4f606
Stephen Blais <stephen@s-blais.com>
2019-10-10 17:43:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,8 +47,8 @@ class Song
   end
 
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} #{col_names_for_insert} VALUES (?)", [values_for_insert])
-        # I don't understand the need for brackets in previous line around values_for_insert
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
